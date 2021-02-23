866a5797074ecca4291b9fc929af33856d77f83c
brettawaytoday <brettawaytoday@gmail.com>
2021-02-24 07:22:43 +1300

    Implemented School struct

---
+++ EMLEngine/School.swift
@@ -0,0 +1,17 @@
+//
+//  School.swift
+//  EMLEngine
+//
+//  Created by Brett Christian on 24/02/21.
+//
+
+import Foundation
+
+struct School {
+    
+    let name: String
+    
+    init(_ name: String) {
+        self.name = name
+    }
+}
--- EMLEngineTests/EMLEngineTests.swift
+++ EMLEngineTests/EMLEngineTests.swift
@@ -9,25 +9,10 @@ import XCTest
 @testable import EMLEngine
 
 class EMLEngineTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    func test_newSchool_hasCorrectName() {
+        let sut = School("school name")
+        
+        XCTAssertEqual(sut.name, "school name")
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
